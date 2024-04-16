79228791ea54e37eef1add3b896a57c2a06c1575
Kartik <kartikgophane2408@gmail.com>
2024-04-16 23:49:03 +0530

    day 1 assign iot

---
+++ day 1/assign1.py
@@ -0,0 +1,13 @@
+
+
+length= float(input("Enter the Length of rectangle :"))
+breadth=float(input("Enter the Breadth of rectangle :"))
+
+peri = 2*(length+breadth)
+area = length*breadth 
+
+
+print(f"Perimeter of rectangle = {peri}")
+print(f"Area  of rectangle = {area}")
+
+
